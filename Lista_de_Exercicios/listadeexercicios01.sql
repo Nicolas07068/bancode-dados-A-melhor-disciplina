4bab0fbc26442cb75d1ad6decd9bd44141d4878d
Nicolas07068 <nicolacocao@gmail.com>
2023-09-11 20:25:49 -0300

    listadeExercicios04.sql

--- Lista_de_Exercicios/listadeexercicios01.sql
+++ Lista_de_Exercicios/listadeexercicios01.sql
@@ -9,3 +9,8 @@ where nascimento < '1900-01-01';
 select * from livros
 where autor_id = 1;
 
+--exercicio 04 
+select alunos.nome, matriculas.curso
+from alunos
+inner join matriculas on alunos.id = matriculas.aluno_id
+where curso = 'Engenharia de Software';
