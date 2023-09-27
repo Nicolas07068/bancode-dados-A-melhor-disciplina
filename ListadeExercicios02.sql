47cac8d3b046ae6f3501a5b3d414d540b93c084c
Nicolas Cardoso <102701493+Nicolas07068@users.noreply.github.com>
2023-09-26 21:03:20 -0300

    listadeExercicios02.sql

--- ListadeExercicios02.sql
+++ ListadeExercicios02.sql
@@ -1,4 +1,4 @@
---exercicios01
+--exercicio 01
 
 delimiter //
 
@@ -10,3 +10,20 @@ end //
 delimiter ;
 
 call sp_ListarAutores();
+
+--exercicio 02
+
+delimiter //
+
+create procedure sp_LivrosPorCategoria(in categoriaNome varchar(100))
+begin
+  select Livro.Titulo
+  from Livro
+  join Categoria on Livro.Categoria_ID = Categoria.Categoria_ID
+  where Categoria.Nome = categoriaNome;
+end //
+
+delimiter ;
+
+
+call sp_LivrosPorCategoria('Ciência');
